eb493ad0b92b5bde254819cccc3955c710163c00
Ryan Ferguson <fergusman@gmail.com>
2017-03-03 18:33:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(words) # put argument(s) here
+  # code here
+  i = 0
+  final = []
+  while i < words.length
+    final.push(yield words[i])
+    i += 1
+  end
+  return final
+end
