d0b17e8d0dff97ccb31592acfac96b8832e0b3d2
Maxwell Benton <maxwellbenton@gmail.com>
2017-03-11 20:24:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  i = 0
+  arr = []
+  while i < collection.length
+    value = yield(collection[i])
+    i += 1
+    arr << value
+  end
+  arr
+end
 
-
+my_collect(collection = "") do |e|
+  e.uppercase
+end
