85c0ad5990f619196d449734cbbe62a4a1a57a48
Jason Sears <jsears4585@gmail.com>
2017-03-15 03:31:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  i = 0
+  new_arr = []
+  while i < array.length
+    new_arr << yield(array[i])
+    i += 1
+  end
+  new_arr
+end
 
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |name|
+  name.split(" ").first
+end
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |lang|
+  lang.upcase
+end
