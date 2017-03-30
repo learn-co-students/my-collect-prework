5d0aed0db76fb921e2fcceedd08f9cf1e6ea3c4d
Gaby Stern <stern.gabrielle@gmail.com>
2017-03-30 12:36:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+languages = ["ruby", "javascript", "python", "objective-c"]
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  new_array = []
+  i = 0
+  while i<array.length
+    new_array.push yield array[i]
+    i = i + 1
+  end
+  new_array
+end
 
+my_collect(languages) do |item|
+  item.upcase
+end
+
+my_collect(students) do |student|
+  student.split(" ").first
+end
