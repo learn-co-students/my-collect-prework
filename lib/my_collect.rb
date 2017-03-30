86012d705d4aa9a4278cd4f0ca7180a980c9a27d
Matthew Hung <matthewbhung101@gmail.com>
2017-03-30 21:35:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect (array)
+  i = 0
+  mod = []
+  while i < array.length
+    new_var = yield array[i]
+    mod.push(new_var)
+        i = i + 1
+  end
+  mod # put argument(s) here# code here
+end
