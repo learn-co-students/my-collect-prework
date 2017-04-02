e53497b0c953aa09d23022692b30e889c791e44a
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-04-02 00:41:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
 
 
+def my_collect(array)
+empty_array = []
+counter = 0
+  while counter < array.length
+    empty_array.push(yield array[counter])
+    counter+=1
+  end
+empty_array
+end
+
+my_collect(languages) {|word| word.upcase}
