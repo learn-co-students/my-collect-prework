a9851c4a0d38b2b4f2a87b833e2529a2600ec468
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-04-09 20:41:21 -0500

    this is my collect,

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    array_to_return = []
+    while i < array.length
+      array_to_return.push(yield array[i])
+      i += 1
+    end
+    array_to_return
+  else
+    nil
+  end
+end
