c087814cc7d5de224c71ff8f9a0cd69cd4b156f1
eli goldstein <elisings356@gmail.com>
2017-05-11 19:16:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+
+students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+
+def my_collect(array)
+  to_print = []
+array.each do |item|
+    x = yield(item)
+    to_print << "#{x}"
+  end
+  return to_print
+end
 
 
+my_collect(collection) do | item |
+    item.upcase
+  end
+
+  my_collect(students) do |student|
+    student.split(" ").first
+  end
