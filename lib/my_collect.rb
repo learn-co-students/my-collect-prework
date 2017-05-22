1145210cdb7564a59d5cf90c45da49090c2ce4ef
Brigit Rossbach <brigitrossbach@gmail.com>
2017-05-22 23:57:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,4 @@
-def my_each(collection)
-    i = 0
-    while i < collection.size
-      yield( collection[i] )
-      i+=1
-    end
-    return collection
-  end
+
 
 def my_collect (array)
   i=0
