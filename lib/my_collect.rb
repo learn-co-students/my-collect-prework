5b6261f4cef1fb94180bd72afd57d70b52359427
dhakle <vacarlos_alm@hotmail.com>
2017-06-12 00:56:19 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+@array = []
+
+def my_collect(array)
+
+	@array = array
+
+	i = 0
+
+	arr = []
+
+	while i < array.length
+		arr << yield(array[i])
 
 
+		i += 1
+
+
+	end
+	
+	arr
+
+end
+
+my_collect(@array) {|el| el.split(" ")[0]}
