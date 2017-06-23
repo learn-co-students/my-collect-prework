6967c46f56ffd63b402681f8b52271b38b1f9373
Daniel Shiling <dshiling21@gmail.com>
2017-06-23 22:00:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_arr = Array.new
 
-
+    while i < array.length
+      new_arr[i] = yield(array[i])
+      i += 1
+    end
+    new_arr
+  else
+    array
+  end
+end
