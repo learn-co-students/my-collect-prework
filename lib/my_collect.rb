af3a015c898191c65c1c58b7eb18d3d217aafeaa
David Goodman <davegoodman20@gmail.com>
2017-07-19 03:22:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+test_arr = ["dave","brian","cato","dillon"]
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
-
+my_collect(test_arr) {|el| puts el.upcase}
