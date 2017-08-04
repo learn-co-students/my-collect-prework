8f86e1e1f2f32609a9efd3e3fd372da2fbdc033f
Al Artesona <artesonaa2@gmail.com>
2017-08-04 00:53:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  empty_array =[]
+  while counter < collection.length
+    empty_array.push(yield collection[counter])
+    counter = counter + 1
+  end
+  empty_array
+end
