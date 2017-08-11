983a37956e4ad230114c593ed7bd8b17b31c551f
William Scripps <wlscripps@gmail.com>
2017-08-11 05:22:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  if block_given?
+    result = array.collect {|x| x}
+    i = 0
+    while i < result.length
+      result[i] = yield result[i]
+      i += 1
+    end
+    return result
+  else
+    puts "No block!"
+  end
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno" ]
+my_collect(array) do |name|
+  name.split.first
+end
