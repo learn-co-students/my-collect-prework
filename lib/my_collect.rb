b95f0e922d0d97c822004bbaedf627afb85078b4
Joshua Denenberg <j.denenberg42@gmail.com>
2017-08-23 14:40:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  idx = 0
+  new_array = []
+  while idx < array.length
+    new_array << yield(array[idx])
+    idx += 1
+  end
+  return new_array
+end
 
-
+my_collect(["Ruby", "Javascript", "Python", "Objective-c"]) do |name|
+  if name.length > 1
+    name.split(" ").first
+  else
+    name.upcase
+  end
+end
