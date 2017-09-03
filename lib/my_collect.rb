f9d4815c8c0b642ac64da15fcbd5a1477239728e
Dick Ward <dick@thedickward.com>
2017-09-03 16:10:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+array = ["tooko", "droozer", "snork"]
 
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array.push(yield array[i])
+    i += 1
+  end
+  new_array
+end
 
+my_collect(array) do |name|
+  name
+end
