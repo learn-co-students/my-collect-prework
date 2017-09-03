0cc29d7e775c929f44cb0b847f6e12c8f0b20cbe
Abe Rudy <aberudy@gmail.com>
2017-09-03 10:27:31 -0400

    first try yall

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+    i = 0
+    new_array = []
+    
+    while i < array.size
+        new_item = yield array[i]
+        new_array << new_item
+        i += 1
+        end
+    new_array
+end
+    
 
