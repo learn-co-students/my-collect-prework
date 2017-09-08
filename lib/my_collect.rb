804a5222363e6fc9179c26d1f8fa3f18829dc29c
Nick Hall <nickhall122@gmail.com>
2017-09-08 14:03:06 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect (my_array)
+i=0
+newarray=[]
+  while i < my_array.length
 
+    newarray << yield(my_array[i])
+    i= i + 1
+  end
+newarray
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  puts name.split(" ").first
+
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  puts lang.upcase
+
+end
