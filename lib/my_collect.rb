57ae4be0d201c2253d016a578409fafd19f0d448
George Gore <georgelgore@gmail.com>
2017-09-27 18:41:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  # output = []
+  # i = 0
+  # while i < array.length
+  #   output << yield array[i]
+  #   i += 1
+  # end
+  #
+  # output
+  array.map{|el| yield el}
+end
