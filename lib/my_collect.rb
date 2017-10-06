e16f1729f0f074908acf785decb3558888104af4
github username <github email address>
2017-10-05 20:29:51 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if block_given?
+      i = 0
+      newCollection = []
+      while i < array.length
+        newCollection<< yield(array[i])
+        i = i + 1
+      end
+      newCollection
+  else
+  end
+end
