67ab3bd23e14b6de1650a307c9f3a901d1e10766
JoshD1 <joshdeiner75@gmail.com>
2017-10-15 16:10:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+module Enumerable
+  def kollect
+    new_ary = []
+    self.each do |elem|
+      new_ary << yield(elem)
+    end
+    new_ary
+  end
+end
+
+names = ["leigh", "marian"]
 
 
+puts names.kollect{ |name| name.upcase }.inspect
+# ["LEIGH", "MARIAN"]
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+puts array.kollect{ |array| array.split(" ").first}.inspect 
