eaec2635eeb93681cb33714670ede7438e1c6382
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 13:18:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
+def my_collect(array)
+    i = 0
 
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+
+    array
+end
+
+my_collect(students) do |student|
+  student.split(" ").first
+end
