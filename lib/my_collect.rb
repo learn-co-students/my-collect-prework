1a829ea86a680c9bf190b807917c91a08cb08e62
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 15:34:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,23 +1,15 @@
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
-
+# languages = ['ruby', 'javascript', 'python', 'objective-c']
+# students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+# newArr = []
 def my_collect(array)
     newArr = []
     i = 0
 
     while i < array.length
       yield array[i]
-      newArr[i] = array[i]
-      i = i + 1
+      newArr.push(array[i])
+      i += 1
     end
 
     newArr
-end
-
-my_collect(languages) do |lang|
-  lang.upcase
-end
-
-my_collect(students) do |student|
-  student.split(' ').first
 end
