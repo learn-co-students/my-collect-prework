a4f38dbb8b6c238be253884fa2b04f365f767228
Christopher Diep <diep.christopher@gmail.com>
2017-12-09 10:32:18 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  while counter < collection.size
+    yield collection[counter]
+    if collection[counter].include? " "
+      new_collection << collection[counter].split(" ").first
+    else
+      new_collection << collection[counter].upcase
+    end
+    counter += 1
+  end
+  new_collection
+end
