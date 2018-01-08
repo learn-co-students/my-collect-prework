8a9104c508502ffb94a955f9fbef1b445b39ac8d
Jason Lim <jayugc@gmail.com>
2018-01-08 05:27:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  i = 0
+  new_array = []
 
+  while i < array.length
+    yielded = yield(array[i])
+    new_array << yielded
+    i += 1
+  end
+  new_array
+end
 
+words = ['hi', 'hello', 'yo', 'sup']
+
+my_collect(words) do |word|
+  word.upcase
+end
+
+twos = ['donkey kong', 'diddy kong', 'dixie kong']
+
+my_collect(twos) do |two|
+  two.split(" ").first
+end
