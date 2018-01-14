1022b258f0001f4a30629b18d2bfa9331960519a
Barney Fabian <bafa@tutanota.de>
2018-01-14 17:52:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  new = []
+  x = 0
+  while x < array.length
+    new << yield(array[x])
+    x += 1
+  end
+  new
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name| name.split(" ").first end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang| lang.upcase end
