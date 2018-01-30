419c9da005ae99d43bbd3ba4a42b104649b46d15
github username <github email address>
2018-01-29 19:34:40 -0500

    I made my own collect method

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if array.length > 0
+    i = 0
+    new_array = []
+    while i < array.length
+      new_array.push(yield array[i])
+      i += 1
+    end
+  end
+  return new_array
+end 
