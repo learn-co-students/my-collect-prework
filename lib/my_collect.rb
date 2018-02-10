b99a53a63cec7daac9f9cf20a31c31d284fd9a33
victoria-huang <vctrah@gmail.com>
2018-02-09 21:52:25 -0500

    collect method

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+    new_array = []
+    i = 0
+    
+    while i < array.length
+        ele = yield array[i]
+        new_array << ele
+        i += 1
+    end
+    
+    new_array
+end
 
