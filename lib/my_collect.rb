7183cc5c3ed95af669660ebdcfe50e6541f65f34
Roger Perez <rogercodes1@gmail.com>
2018-02-15 06:58:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,33 @@
+def my_collect(array)
+  i=0
+  arr=[]
+  upper_case=[]
+  first_name=[]
+  while i< array.size
+    if array[i].split(" ").first == array[i].split(" ").first.capitalize
+      arr<< array[i].split(" ").first
+    else
+      arr<< array[i].upcase
+    end
+    i= i+1
+  end
+  
+  arr
+end 
 
 
+
+
+
+
+
+# def other_my_collect(array)
+#   i=0
+#   upper_case=[]
+#   first_name=[]
+#   while i< array.size 
+#     yield array[i]
+#     i= i+1
+#   end
+#   array
+# end 
