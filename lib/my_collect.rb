beb799597dc6ca33a050675c8b38096b2209d51b
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-09 22:41:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if block_given?
+    output = []
+    i = 0
+    while i < array.length
+      output.push(yield(array[i]))
+      i = i + 1
+    end
+    output
+  end
+end
