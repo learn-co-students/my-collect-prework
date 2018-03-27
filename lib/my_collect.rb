98b389efa26bc45164d02305f2795d2452bae365
github username <github email address>
2018-03-26 20:45:03 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+    if array == languages
+      array.collect do |x| x.upcase
+      end
 
+    else
+      array.collect do |x| x.split(" ").first
+      end
 
+    end
+end
