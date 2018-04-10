ffab3a708511720cffe346c10c7c809d68734a6d
jcr2125 <email>
2018-04-10 16:47:25 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection) # put argument(s) here
+  # code here
+  i = 0
+  new_collection = []
 
+  while i < collection.length
+    new_collection.push(yield collection[i])
+    i += 1
+  end
 
+  new_collection
+end
