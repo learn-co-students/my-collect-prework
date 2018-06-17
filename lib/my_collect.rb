478fb6adebdb0ee9dd10c46515b8fd090c1a0a94
whynotski <laurawynosky@gmail.com>
2018-06-16 20:21:16 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+student = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection =["ruby", "javascript", "python", "objective-c"]
 
+def my_collect(array)
+  i = 0
+  new_collect = []
 
+  while i < array.length
+    new_collect.push yield (array[i])
+    i += 1
+  end
+
+new_collect
+end
+
+my_collect(student) do [name]
+  name.split(" ").first
+end
+
+my collect(collection) do |lang|
+  lang.upcase
+end
