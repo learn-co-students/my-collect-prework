19aa28049a1858e8a52769f12f4d7f2eb1de3c09
Your Name <you@example.com>
2018-06-21 09:46:10 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+i = 0
+new_array = []
+#above we have our counter and and empty array store our new values
+while i < array.length
+new_array << yield(array[i])
+#we are iterating through the array, and push each element into empty array
+i+=1
+end
+new_array
+end
