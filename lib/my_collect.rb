33b728f1cfb9f72c39aeff9d1cfef7708390e8e2
github username <github email address>
2018-07-05 20:19:27 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  counter = 0
 
-
+  # collection.map do |collect|
+  #   yield(collect.upcase)
+  # end
+  new_array = []
+  while counter < collection.length
+    new_array.push yield(collection[counter])
+    counter += 1
+  end
+  new_array
+end
