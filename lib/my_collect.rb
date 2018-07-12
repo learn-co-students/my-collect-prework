7f4d701f6e7e9e8c5c1df733245731f5c57f49f0
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-12 20:22:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   counter = 0
   newer_array = []
   while counter < array.length
-    yield(words[counter])
+    newer_array << yield(array[counter])
     counter += 1
   end
   return newer_array
