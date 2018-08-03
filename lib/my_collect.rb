002ec0ae78cafca5f94deae5d4b7b59e40cf89d5
rogerdavidvera <rovera@vassar.edu>
2018-08-03 12:26:16 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  index = 0
+  output_array = [];
+  while index < collection.size do
+    output_array[index] = yield(collection[index])
+    index += 1
+  end
+  output_array
+end
