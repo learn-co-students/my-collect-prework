39d7294a6e4ea0627f0d4afc0203a5df31b1c41c
Jin Ai Yap <jinai.jyap@gmail.com>
2018-08-04 14:29:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    new = []
+    while i < array.length do
+      # yield array[i]
+      new.push(yield array[i])
+      i += 1
+    end
+    new
+  else
+    puts "No block given"
+  end
+end
