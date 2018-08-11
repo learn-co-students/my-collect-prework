7e5f3b0bf403c832f97716f4c8da564dadabbf6d
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-11 14:03:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+def my_collect(array)
+ i = 0
+ new_array = []
+  while i < array.length 
+    new_array.push yield array[i]
+    i += 1
+  end
+  new_array
+end 
+  
+  
+  def using_push(array, string)
+  array.push(string)
+end
+  
+  
+  def my_map(array)
+  new_array = []
 
+  for element in array
+    new_array.push yield element
+  end
 
+  new_array
+end
