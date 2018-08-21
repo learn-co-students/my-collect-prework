2df9e803280365b9b44bdb40d69cc352121417f9
Srikant Kumar Kalaputapu <sriki@Srikants-MBP.home>
2018-08-21 15:22:27 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
 
+def my_collect(arr)
+    if block_given?
+      i = 0
+      arrnew = []
+      while i < arr.size
+         arrnew.push(yield arr[i])
+          i = i + 1
+      end
+      arrnew
+    else
+        "No block given"
+    end
+
+end
