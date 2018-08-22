c8f633e45a915470a0ac0116ec1d36f4ea1544f0
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 22:30:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  i = 0
+  array2 = []
+  
+  while i < array.length
+    array2.push(yield array[i])
+    i += 1
+  end
+  
+  array2
+end
 
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(students) do |name|
+  name.split(" ").first
+end
 
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |lang|
+  lang.upcase
+end
