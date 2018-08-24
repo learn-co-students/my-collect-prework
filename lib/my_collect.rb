1b11d08a58f8eda7b087c7c774098b457cec3827
sparkbold-git <sparkboldstudio@gmail.com>
2018-08-24 16:01:00 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  new_array = []
+  if array.size != 0
+    i = 0
+    while i < array.size
+      new_array[i] =yield array[i]
+      i+=1
+    end
+  end
+  new_array
+end
