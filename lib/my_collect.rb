aeb76921d2a8b5e4788d0b1f9702e0612af8d823
Mirco guidetti <mirco@guidetti.co>
2018-09-05 17:34:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+ def my_collect (array)
+   array.collect do |name|
+     yield name
+   end
+ end
+ 
+      
 
+languages = ['ruby', 'javascript', 'python', 'object-c']
 
+my_collect(languages) do |language|
+  puts language.upcase
+end
+ 
