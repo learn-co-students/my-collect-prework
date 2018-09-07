be53cf1ba1d42897b868627aa04dbf0f2d2aee45
github username <github email address>
2018-09-06 21:57:43 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(collection)
+  i = 0
+  changes = [] #remember that collection and map
 
-
+  while i < collection.length
+    changes << yield(collection[i])
+    i += 1
+  end
+  changes
+end
