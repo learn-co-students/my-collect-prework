fb61566f9d19e6d7ca68501bb66849f10b817d61
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-09-24 20:27:17 +0100

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection)
+  i = 0
+  modified_collection = []
 
+  while i < collection.length
+    modified_element = yield(collection[i])
+    modified_collection << modified_element
+    i += 1
+  end
 
+  modified_collection
+end
