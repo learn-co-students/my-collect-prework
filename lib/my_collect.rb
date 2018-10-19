61fc6043dda74c1db676e09262a4d164c981259b
joneseyit <joneseyit@protonmail.com>
2018-10-19 23:17:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    x =  yield array[i]
+    new_array << x
+    i += 1
+  end
+  return new_array
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(array) do |name|
+  name.split(" ").first
+end
