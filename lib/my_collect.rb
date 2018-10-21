007a5f6ef468a00ba79ead472930721507a330e1
demiansims <demiansims@gmail.com>
2018-10-21 14:27:39 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(collection)
+  new_collection = []
+  if collection.length == 0
+    collection
+  else
+
+  i = 0
+
+  while i < collection.length
+   new_collection.push(yield(collection[i]))
+
+    i += 1
+ end
 
 
+  return new_collection
+
+end
+end
