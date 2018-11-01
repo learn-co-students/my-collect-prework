b55470a0e8759126c9efe2fc5412b9af1ede36fd
github username <github email address>
2018-11-01 14:57:48 -0400

    My Collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,3 @@
-#def my_collect(collection)
-#  i = 0
-#  new_collection = []
-#  while i < collection.length
-#    yield collection[i]
-#    i + 1
-#    collection.each do |lang|
-#      lang.upcase
-#    end
-#  end
-#  new_collection(collection)
-#end
-
 def my_collect(collection)
   i = 0
   new_collection = []
