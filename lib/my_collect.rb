eb0a8c042e6963f532f7358086d8b7f7b59cb522
dagonyclark <sean.dagony-clark@flatironschool.com>
2018-11-04 20:49:28 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  collection_mod = []
+  while counter < collection.length
+    collection_mod[counter] = yield(collection[counter])
+    counter += 1
+  end
+  collection_mod
+end
