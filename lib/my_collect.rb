b90de8d0a2584b64572e8141af6de75c2167f888
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-12 19:12:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  i = 0
+  empty_array = []
+  while i < collection.length 
+    new_value = yield collection[i]
+    empty_array << new_value
+    i += 1 
+  end
+  empty_array
+end
