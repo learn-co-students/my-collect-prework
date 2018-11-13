2d7b8148edf1f13d042aa2aab32e0e93e199bde6
tfuquirk <timothyfquirk@gmail.com>
2018-11-13 09:12:13 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
 
+def my_collect(array)
+  i = 0
+  modified_array = []
+
+  while i < array.length
+    modified_array.push(yield(array[i]))
+
+    i += 1
+  end
+  modified_array
+end
+
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |modify|
+  modify.upcase
+end
