aee44a9797e84edf278be1841637e775c35edf36
davidfloyd91 <davidfloyd91@gmail.com>
2018-11-26 01:11:33 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+array = []
 
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0
 
+    while i < array.length
+      new_array.push(yield(array[i]))
+      i += 1
+    end
+
+    new_array
+  end
+end
