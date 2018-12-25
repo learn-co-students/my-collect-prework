15f0b9c410f58e64618df81d7ef7ca2321aa0c3f
Anthony Gregg <agregg22@gmail.com>
2018-12-25 01:55:10 -0500

    Complete

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
 
+  new_arr =[]
+  counter = 0
 
+if array.include?("ruby")
+  while counter < array.length
+    new_arr << array[counter].upcase
+    counter += 1
+  end
+  new_arr
+else
+
+  while counter < array.length
+    new_arr << array[counter].split(" ").first
+    counter += 1
+  end
+  new_arr
+end
+end
