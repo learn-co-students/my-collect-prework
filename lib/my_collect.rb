73bf41b00c850b7976d2f0aca351bfbb9e6f5d61
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-20 02:23:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
 
-
+def my_collect(collection)
+   collectionTwo = [] 
+  i = 0 
+  while i < collection.length 
+    collectionTwo.push(yield(collection[i]))
+    i += 1 
+  end 
+  collectionTwo
+end 
+ 
