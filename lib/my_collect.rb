5395dfc13f04f2462a7f9476f23f816cdab02717
stephenyoon84 <stephenyoon84@gmail.com>
2019-01-28 13:43:08 -0500

    My-collect - Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    changed_element = yield array[i]
+    new_array << changed_element
+    i += 1
+  end
+  return new_array
+end
