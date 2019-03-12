ab3c09df2382c13485d0f09a36f9b41201e8d114
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-12 16:47:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  i = 0 
+  returned_info =[]
+  if block_given?
+    while i < array.length 
+    returned_info << yield(array[i])
+      i =i + 1 
+    end 
+  end
+  returned_info
+end 
 
