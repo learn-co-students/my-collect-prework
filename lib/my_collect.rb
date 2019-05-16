cba7c7d54eeb9a411ccb77ac21cea50f14e947fa
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 02:40:44 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
 
 
+def my_collect (array)
+  i = 0
+  result = []
+  while i < array.length
+  result <<  yield(array[i])
+    i = i+1
+  end
+  result
+end
+
+#my_collect(array) do |i|
+#  i.split(" ").first
+#end
+
+#my_collect(array) do |i|
+#  i.upcase
+#end
