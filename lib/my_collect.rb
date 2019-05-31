41b5c4808ebbaefe719ccf42bd8fee24f67da159
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-31 22:00:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end  
 
-
+array = ["ruby", "javascript", "python"]
+my_collect(array) do |name|
+  name.upcase
+end
