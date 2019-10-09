801690c9e795339c1e3a1839b3682198c0d22889
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:41:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  i = 0
+  first_name = []
+  while i < array.size do
+    first_name.push(yield(array[i])) if block_given?
+    i += 1
+  end
 
+  first_name
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
