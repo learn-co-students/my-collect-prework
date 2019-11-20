64c4c502c825ddbb7b66846e4b1671f53df7f3b5
dagonyclark <sean@dagony-clark.com>
2019-11-20 09:57:23 -0500

    Revert "Done."
    This reverts commit eb0a8c042e6963f532f7358086d8b7f7b59cb522.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,2 @@
-def my_collect(collection)
-  counter = 0
-  collection_mod = []
-  while counter < collection.length
-    collection_mod[counter] = yield(collection[counter])
-    counter += 1
-  end
-  collection_mod
-end
+
+
