6403cc0d9b40c8f485839b6b90ef3512ffef4d0e
dagonyclark <sean@dagony-clark.com>
2019-11-20 10:07:23 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+    counter = 0
+    languages = []
+    while array.size > counter do
+        languages << yield(array[counter])
+        counter += 1
+    end
+    languages
+end
 
-
+my_collect(2) do |item| 
+    item
+end
